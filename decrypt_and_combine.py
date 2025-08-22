a6aa4356111bdf2b84dd0ddb7e18d8608a76d434
asl97 <asl97@airmail.cc>
2025-08-23 06:29:54 +0800

    Use timestamps as name for generated mp4/apng

--- decrypt_and_combine.py
+++ decrypt_and_combine.py
@@ -1,8 +1,12 @@
 import os
 
-for i, png in enumerate([l for l in os.listdir() if l.endswith('.gpg')]):
+files = [l for l in os.listdir() if l.endswith('.gpg')]
+
+for i, png in enumerate(files):
     os.system(f'gpg  --quiet --batch --yes --decrypt --passphrase="tiles" --output {"%05d.png"%i} {png}')
 
-os.system(r'ffmpeg -r 3 -i "%05d.png" -c:v libx264 -qp 0 out.mp4')
-os.system(r' ffmpeg -r 3 -i "%05d.png" -plays 0 out.apng')
+os.system(r'ffmpeg -r 3 -i "%05d.png" -c:v libx264 -qp 0 '+f'{files[0][2:-8]}_{files[-1][2:-8]}.mp4')
+#ffv1 result in giant file
+#os.system(r'ffmpeg -r 3 -i "%05d.png" -c:v ffv1 '+f'{files[0][2:-8]}_{files[-1][2:-8]}.mkv')
+os.system(r' ffmpeg -r 3 -i "%05d.png" -plays 0 '+f'{files[0][2:-8]}_{files[-1][2:-8]}.apng')
 # ffmpeg -r 3 -i "%05d.png" -vf "crop=270:280:0:175" -y -plays 0 out.apng
