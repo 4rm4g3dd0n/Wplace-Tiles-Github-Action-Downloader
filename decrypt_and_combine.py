143dc022deac53c55b8e72d9d10775c906db8344
asl97 <asl97@airmail.cc>
2025-08-24 06:50:40 +0800

    Add stitching tile side by side before outputting timelapse

--- decrypt_and_combine.py
+++ decrypt_and_combine.py
@@ -1,9 +1,31 @@
 import os
 
 files = [l for l in os.listdir() if l.endswith('.gpg')]
+pngs = []
 
 for i, png in enumerate(files):
-    os.system(f'gpg  --quiet --batch --yes --decrypt --passphrase="tiles" --output {"%05d.png"%i} {png}')
+    pngs.append(png[:-4])
+    os.system(f'gpg  --quiet --batch --yes --decrypt --passphrase="tiles" --output {png[:-4]} {png}')
+
+dpngs = {}
+for png in pngs:
+    i = png.split("_")[0]
+    if i not in dpngs: dpngs[i] = []
+
+    dpngs[i].append(png)
+
+if len(dpngs) > 1:
+    png_group = [l for l in zip(*dpngs.values())]
+    for group in png_group:
+        assert group[0].split("_")[-1] == group[1].split("_")[-1]
+
+    for i, group in enumerate(png_group):
+        # combine image side by side, no config/option to easily set layout yet
+        os.system(f'convert {" ".join(group)} +append {"%05d.png"%i}')
+else:
+    for i, png in enumerate(*dpngs.values()):
+        os.rename(png, '%05d.png'%i)
+
 
 os.system(r'ffmpeg -r 3 -i "%05d.png" -c:v libx264 -qp 0 '+f'{files[0][2:-8]}_{files[-1][2:-8]}.mp4')
 #ffv1 result in giant file
